dbc3f29a48865ec967eeed8e93a75493df6c700c
Juliia Haidai <94704941+JuliiaHaidai@users.noreply.github.com>
2022-12-01 20:33:03 +0200

    fix performance issues

--- components/FormInput.tsx
+++ components/FormInput.tsx
@@ -1,21 +1,30 @@
+import { memo } from "react";
+
 type Props = {
-  id: string,
-  label: string,
-  onInput: (input: string) => void,
-  errorMessage?: string,
-}
+  id: string;
+  label: string;
+  onInput: (input: string) => void;
+  errorMessage?: string;
+};
 
 function wait(milliseconds: number) {
   const start = new Date().getTime();
   while (new Date().getTime() - start < milliseconds) {}
 }
-export default function FormInput({id, label, errorMessage, onInput}: Props) {
+export default memo(function FormInput({
+  id,
+  label,
+  errorMessage,
+  onInput,
+}: Props) {
   // This component is a bit slow to render...
   wait(25);
 
   return (
     <div className="flex items-center">
-      <label htmlFor={id} className="flex-1">{label}</label>
+      <label htmlFor={id} className="flex-1">
+        {label}
+      </label>
       <div className="flex-1">
         <input
           type="text"
@@ -23,9 +32,10 @@ export default function FormInput({id, label, errorMessage, onInput}: Props) {
           name={id}
           className="w-full border rounded p-2"
           onInput={(e) => onInput(e.currentTarget.value)}
+          //  onBlur={(e) => onInput(e.currentTarget.value)}
         />
         {errorMessage && <p className="text-sm text-red-600">{errorMessage}</p>}
       </div>
     </div>
   );
-}
+});
