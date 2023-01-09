ee75045b1d6d302b46680ece966624a575849ea3
Sergii Kushnerov <serhii.kushnerov.dev@gmail.com>
2023-01-09 17:20:44 +0200

    add button

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,10 +17,15 @@ export default function Home() {
       </Head>
 
 
-      <div style={{ display: "flex", border: "3px solid red"}}>
-        <Button />
-        <Button size={'small'} type={'button'} color="green" text="Button one" />
-        <Button size={'large'} text="Button two"/>
+      <div>
+      <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Default</button>
+        <Button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" text="NEW Buuton" />
+        <Button
+        text="Third button"
+        className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" />
+        <Button 
+        className="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900"
+        size={'large'} text="Button two"/>
       </div>
 
  {/*
