a1be6f55909bf83fc75b51c79d8b1cc562828e0f
Sergii Kushnerov <serhii.kushnerov.dev@gmail.com>
2023-01-17 17:03:07 +0200

    add types on MockData:

--- mock/MockData.tsx
+++ mock/MockData.tsx
@@ -2,8 +2,18 @@ export interface TypeMock {
   bgColor: string[];
   value?: any;
   children?: React.ReactNode;
+  errorData?: {
+    errorNumber: string;
+    errorMessage: string;
+    errorBack: string;
+  };
 }
 
 export const MockData: TypeMock = {
   bgColor: ["green", "blue"],
+  errorData: {
+    errorNumber: "404",
+    errorMessage: "Page Not Found",
+    errorBack: "Go Home",
+  },
 };
--- pages/404.tsx
+++ pages/404.tsx
@@ -1,24 +1,27 @@
 import Link from "next/link";
 import React from "react";
+import { MockData, TypeMock } from "../mock/MockData";
 
 export default function Error() {
+  const errorData = { ...MockData.errorData };
+
   return (
     <div>
       <div className="h-screen w-full flex flex-col justify-center items-center bg-[#1A2238]">
         <h1 className="text-9xl font-extrabold text-white tracking-widest">
-          404
+          {errorData.errorNumber}
         </h1>
         <div className="bg-[#FF6A3D] px-2 text-sm rounded rotate-12 absolute">
-          Page Not Found
+          {errorData.errorMessage}
         </div>
         <button className="mt-5">
-          <a className="relative inline-block text-sm font-medium text-[#FF6A3D] group active:text-orange-500 focus:outline-none focus:ring">
+          <div className="relative inline-block text-sm font-medium text-[#FF6A3D] group active:text-orange-500 focus:outline-none focus:ring">
             <span className="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-[#FF6A3D] group-hover:translate-y-0 group-hover:translate-x-0"></span>
 
             <span className="relative block px-8 py-3 bg-[#1A2238] border border-current">
-              <Link href="/">Go Home</Link>
+              <Link href="/">{errorData.errorBack}</Link>
             </span>
-          </a>
+          </div>
         </button>
       </div>
     </div>
